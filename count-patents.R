8c204c547105e57d1003a3104367b288ee4c9b1e
trriley <im.triley@gmail.com>
2020-01-06 12:35:16 -0600

    add comments

--- count-patents.R
+++ count-patents.R
@@ -2,10 +2,17 @@ require(tidyverse)
 require(patentsview)
 require(here)
 
-fieldsTbl <- as_tibble(fieldsdf) %>%
-  filter(endpoint == "patents")
+# this pulls a table of fields and their descriptions which can be called
+# to identify any other fields of interest
+fieldsTbl <- tibble::as_tibble(fieldsdf) %>%
+  dplyr::filter(endpoint == "patents")
 
-query <- with_qfuns(
+# the query is defined here as it will be the same in each of the 3 searches
+#
+# with_qfuns is a helper function used by patentsview to build a query for
+# the patentsview API. if you enter query into the command line you'll see
+# the gobbledygook it generates
+query <- patentsview::with_qfuns(
   and(
     eq(patent_firstnamed_inventor_state = "IL"),
     or(
@@ -16,9 +23,22 @@ query <- with_qfuns(
   )
 )
 
+# I am wrapping this in a function because somebody told me that is good
+# practice
 countPatents <- function() {
-  fields <- c("patent_number", "patent_year", "patent_date", "patent_firstnamed_inventor_city", "patent_firstnamed_inventor_state")
   
+  # these are the fields which the search will return
+  fields <- c(
+    "patent_number",
+    "patent_year",
+    "patent_date",
+    "patent_firstnamed_inventor_city",
+    "patent_firstnamed_inventor_state"
+  )
+  
+  # the following lines execute the search and then process the table
+  # the %>% operator takes the output of the previous function and 
+  # "pipelines" it through as the first argument of the next function
   pv_res <- search_pv(query, fields, endpoint = "patents", all_pages = TRUE) %>%
     as_tibble() %>%
     select(data) %>%
@@ -32,8 +52,17 @@ countPatents <- function() {
     bind_rows(bind_cols(city = "Bloomington-Normal", n = sum(pv_res$n)))
 }
 
+# the other two functions work using the same logic as the first
 countPatentsByOrg <- function() {
-  fields <- c("assignee_organization", "patent_number", "patent_year", "patent_date", "patent_firstnamed_inventor_city", "patent_firstnamed_inventor_state")
+  
+  fields <- c(
+    "assignee_organization",
+    "patent_number",
+    "patent_year",
+    "patent_date",
+    "patent_firstnamed_inventor_city",
+    "patent_firstnamed_inventor_state"
+  )
   
   pv_res <- search_pv(query, fields, endpoint = "patents", all_pages = TRUE) %>%
     as_tibble() %>%
@@ -48,7 +77,14 @@ countPatentsByOrg <- function() {
 }
 
 getPatentTable <- function() {
-  fields <- c("assignee_organization", "patent_number", "patent_date", "patent_title", "patent_abstract")
+  
+  fields <- c(
+    "assignee_organization",
+    "patent_number",
+    "patent_date",
+    "patent_title",
+    "patent_abstract"
+  )
   
   pv_res <- search_pv(query, fields, endpoint = "patents", all_pages = TRUE) %>%
     as_tibble() %>%
@@ -59,6 +95,8 @@ getPatentTable <- function() {
     
 }
 
+# these lines call each function, store the output as objects in R and also
+# in .csv files
 patentCount <- countPatents() %>%
   write_csv(here("patent-count-by-city.csv"))
 
